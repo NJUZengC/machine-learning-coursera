73d57683089ec0e6f4873045b8422119f4a7d183
zengc <njuzengc@foxmail.com>
2018-05-22 18:11:42 +0800

    Anomaly Detection and Recommender System ex8 finished

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,8 +40,25 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+J = sum(sum(((X * Theta' - Y) .* R) .^ 2))/2;
 
+J = J +  (lambda/2) * (sum(sum(X .^ 2)) + sum(sum(Theta .^ 2)) );
 
+for i=1:num_movies
+  idx = find(R(i,:) == 1);
+  Theta_temp = Theta(idx,:);
+  Y_temp = Y(i,idx);
+  
+  X_grad(i,:) = (X(i,:) * Theta_temp' - Y_temp) * Theta_temp + lambda*(X(i,:));
+end
+
+for i=1:num_users
+  idx = find(R(:,i) == 1);
+  X_temp = X(idx,:);
+  Y_temp = Y(idx,i);
+  
+  Theta_grad(i,:) =(Theta(i,:) * X_temp' - Y_temp') * X_temp + lambda * (Theta(i,:));
+end
 
 
 
